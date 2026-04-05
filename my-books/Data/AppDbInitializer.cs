e3bdbd244c7e9addaf772e90aca5f58562df7673
Jorge Cabrera Souto <jorgecabrerasouto@outlook.com>
2026-04-04 22:04:00 -0500

    Commit Section 3: Building Your First Asp.Nep API

---
+++ my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+using System;
+using System.Linq;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed (IApplicationBuilder applicationBuilder)
+        {
+            using var serviceScope = applicationBuilder.ApplicationServices.CreateScope();
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(
+                    new Book()
+                    {
+                        Title = "JAVASCRIPT & JQUERY",
+                        Description = "Interactive front-end web development",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-250),
+                        Rate = 4,
+                        Genre = "Web programming",
+                        Author = "Jon Ducket",
+                        CoverUrl = "https:...",
+                        DateAdded = DateTime.Now
+
+                    },
+                    new Book()
+                    {
+                        Title = "HTML & CSS",
+                        Description = "Design and buil websites",
+                        IsRead = false,
+                        Genre = "Web programming",
+                        Author = "Jon Ducket",
+                        CoverUrl = "https:...",
+                        DateAdded = DateTime.Now
+
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
